e7e05ac13dfbecc8d970ce272c4dc903b9b3fc5f
neminx <101298482+CarlosCastanedaDev@users.noreply.github.com>
2023-09-29 15:02:02 -0500

    update routes working

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -29,4 +29,34 @@ class MoviesController < ApplicationController
 
     redirect_to("/movies")
   end
+
+  def destroy
+    the_id = params.fetch("id")
+
+    matching_movies = Movie.where({ :id => the_id})
+
+    the_movie = matching_movies.at(0)
+
+    the_movie.destroy
+
+    redirect_to("/movies")
+  end
+
+  def update
+    the_id = params.fetch("id")
+
+    matching_movies = Movie.where({ :id => the_id})
+
+    the_movie = matching_movies.at(0)
+
+    the_title = params.fetch("the_title")
+    the_year = params.fetch("the_year")
+    the_duration = params.fetch("the_duration")
+    the_description = params.fetch("the_description")
+    the_image = params.fetch("the_image")
+    the_director_id = params.fetch("the_director_id")
+
+    the_movie.update({:title => the_title, :year => the_year, :duration => the_duration, :description => the_description, :image => the_image, :director_id => the_director_id})
+    redirect_to("/movies/#{the_id}")
+  end
 end
